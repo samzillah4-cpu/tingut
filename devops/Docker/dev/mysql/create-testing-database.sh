a2cc502e625e20a04fcfca6a589c31a0815464b2
DennisKainga <denniskainga@gmail.com>
2026-04-23 21:21:20 +0300

    feat:(docker)

--- devops/Docker/dev/mysql/create-testing-database.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
-
-    CREATE DATABASE IF NOT EXISTS test_db;
-    GRANT ALL PRIVILEGES ON \`test_db%\`.* TO '$MYSQL_USER'@'%';
-EOSQL
--- devops/Docker/live/nginx/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-envsubst '${FPM_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-exec nginx -g "daemon off;"
